7b9a8d786c0145c02d302a7e11a0cdcfaaad3f01
Sean McGrath <smcgrath@hsph.harvard.edu>
2025-02-22 15:58:46 -0500

    Added some error catching back in

--- R/learner.R
+++ R/learner.R
@@ -36,7 +36,21 @@
 #'
 #' @export
 cv.learner <- function(Y_source, Y_target, lambda_1_all, lambda_2_all, step_size,
-                       n_folds = 4, max_iter = 100, threshold = 1e-3, n_cores = 1, r = 1) {
+                       n_folds = 4, max_iter = 100, threshold = 1e-3, n_cores = 1, r) {
+  ## Error catching
+  if (!identical(dim(Y_source), dim(Y_target))){
+    stop('Y_source and Y_target must have the same dimensions')
+  }
+  if (any(is.na(Y_source))){
+    stop('Y_source cannot have NA values.')
+  }
+
+  if (missing(r)){
+    p <- nrow(Y_source); q <- ncol(Y_source)
+    max_rank <- min(p, q) / 3
+    r <- max(ScreeNOT::adaptiveHardThresholding(Y = Y_source, k = max_rank)$r, 1)
+  }
+
   result <- cv_learner_cpp(Y_source, Y_target, lambda_1_all, lambda_2_all, step_size,
                            n_folds, max_iter, threshold, n_cores, r)
   return(result)
@@ -55,7 +69,27 @@ cv.learner <- function(Y_source, Y_target, lambda_1_all, lambda_2_all, step_size
 #' @return A list containing the learner estimate, objective values, convergence criterion, and rank.
 #' @export
 learner <- function(Y_source, Y_target, lambda_1, lambda_2, step_size,
-                    max_iter = 100, threshold = 1e-3, r = 1) {
+                    max_iter = 100, threshold = 1e-3, r) {
+  ## Error catching
+  if (!identical(dim(Y_source), dim(Y_target))){
+    stop('Y_source and Y_target must have the same dimensions')
+  }
+  if (any(is.na(Y_source))){
+    stop('Y_source cannot have NA values.')
+  }
+  if (length(lambda_1) > 1){
+    stop('lambda_1 must be a scalar. See the function cv.learner for selecting a suitable lambda_1 value.')
+  }
+  if (length(lambda_2) > 1){
+    stop('lambda_2 must be a scalar. See the function cv.learner for selecting a suitable lambda_2 value.')
+  }
+
+  if (missing(r)){
+    p <- nrow(Y_source); q <- ncol(Y_source)
+    max_rank <- min(p, q) / 3
+    r <- max(ScreeNOT::adaptiveHardThresholding(Y = Y_source, k = max_rank)$r, 1)
+  }
+
   result <- learner_cpp(Y_source, Y_target, r, lambda_1, lambda_2, step_size, max_iter, threshold)
   return(result)
 }
